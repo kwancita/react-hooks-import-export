e2978a70acbad5ba0b756bf8beaa106fa7724461
Wasanta <kwan_u_day@hotmail.com>
2021-11-21 15:07:34 -0500

    done

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,23 @@
 import React from "react";
+//import howManyParks from "./parks/howManyParks";
+//import MesaVerde from "./parks/MesaVerde";
+
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as RMFunctions from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+  // howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  // return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <MesaVerde />
+      <howManyParks />
+      <RMFunctions />
+    </div>
+  );
 }
+
+
+export default ColoradoStateParks;
